3f2be03f844a1ed5bc0e63d1d69eb21f6f5e539e
RafaelBamberg <raafaelbamberg@gmail.com>
2024-07-15 17:38:35 -0300

    adjust pagination

--- src/api/universe/universe.ts
+++ src/api/universe/universe.ts
@@ -1,15 +1,13 @@
 import axios from "axios";
 import * as T from "./types";
 
-const API_URL = "https://rickandmortyapi.com/api/character";
-
 export const getCharacters = async (
   page: number,
-  query: string = ""
+  characterName: string = ""
 ): Promise<T.Character> => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await axios.get<T.Character>(
-    `${API_URL}/?page=${page}&name=${query}`
+    `https://rickandmortyapi.com/api/character/?page=${page}&name=${characterName}`
   );
   return response.data;
 };
